b1b201b7fc6ebd770c567bcb53a32475a62a5159
Eric Spoerner <espoerner@gmail.com>
2021-12-27 10:41:24 -0800

    Add analysis of titles for eligible retirees.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,45 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--TO-DO: FIGURE OUT WHY UNIQUE ENTRIES ARE DIFFERENT THAN EXPECTED IN SCREENSHOT
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+--Challenge part 1
+DROP TABLE IF EXISTS retirement_titles;
+
+SELECT		e.emp_no
+			,e.first_name
+			,e.last_name
+			,t.title
+			,t.from_date
+			,t.to_date
+INTO		retirement_titles
+FROM		employee AS e
+INNER JOIN 	title AS t ON e.emp_no = t.emp_no
+WHERE		DATE_PART('year',e.birth_date) BETWEEN 1952 and 1955
+ORDER BY 	emp_no ASC;
+
+--SELECT * FROM retirement_titles
+
+-- Retrieve all unique retirement-eligible employee names and their titles
+DROP TABLE IF EXISTS unique_title;
+
+select	DISTINCT ON (emp_no) 
+		emp_no
+		,first_name
+		,last_name
+		,title
+INTO	unique_title
+FROM 	retirement_titles rt
+WHERE 	to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+--SELECT * FROM unique_title;
+
+--Determine count of individual titles.  WHY is this different from expected?
+DROP TABLE IF EXISTS retiring_title;
+
+SELECT 		COUNT(*)
+			,title
+INTO		retiring_title
+FROM 		unique_title
+GROUP BY 	title
+ORDER BY 	COUNT(*) DESC;
+
+SELECT * FROM retiring_title;
